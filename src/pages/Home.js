800680c326827db95dfedc789dc120f00a0c7fe9
ginrone <neirobungin@gmail.com>
2023-08-23 23:42:36 +0700

    added template tailwind pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,119 @@
 function Home(){
     return (
-        <h1> Test </h1>
+        <div className="w-[1440px] h-[2722px] bg-white flex-col justify-center items-start inline-flex">
+            <div className="w-[1440px] h-[115px] relative">
+                <div className="w-[1440px] h-[115px] left-0 top-0 absolute bg-neutral-800" />
+                <div className="w-[191px] h-[74px] left-[28px] top-[12px] absolute">
+                <img className="w-[74px] h-[74px] left-0 top-0 absolute" src="https://via.placeholder.com/74x74" />
+                <div className="left-[90px] top-[26px] absolute text-amber-400 text-5xl font-normal">LIFT</div>
+                </div>
+                <div className="left-[449px] top-[46px] absolute text-white text-2xl font-normal">HOME</div>
+                <div className="left-[1021px] top-[46px] absolute text-amber-400 text-2xl font-normal">ABOUT</div>
+                <div className="left-[626px] top-[46px] absolute text-amber-400 text-2xl font-normal">SERVICES</div>
+                <div className="left-[835px] top-[46px] absolute text-amber-400 text-2xl font-normal">PROJECT</div>
+                <div className="w-[78px] h-1 left-[449px] top-[70px] absolute bg-amber-400" />
+            </div>
+            <div className="w-[1440px] h-[487px] relative">
+                <img className="w-[1440px] h-[487px] left-0 top-0 absolute" src="https://via.placeholder.com/1440x487" />
+                <div className="w-[1440px] h-[487px] left-0 top-0 absolute bg-black bg-opacity-60" />
+                <div className="left-[312px] top-[196px] absolute text-white text-[64px] font-normal">CONSTRUCT YOUR DREAM</div>
+                <div className="w-[234px] h-[68px] left-[610px] top-[337px] absolute bg-zinc-600 bg-opacity-0 border border-white" />
+                <div className="left-[642px] top-[355px] absolute text-white text-[32px] font-normal">SEE MORE</div>
+            </div>
+            <div className="w-[1440px] h-[1013px] relative">
+                <div className="w-[1440px] h-[1013px] left-0 top-0 absolute bg-white" />
+                <div className="w-[353px] h-[234px] left-[978px] top-[682px] absolute">
+                <div className="w-[170px] h-[170px] left-[92px] top-0 absolute bg-amber-400 rounded-full" />
+                <img className="w-[104px] h-[104px] left-[116px] top-[33px] absolute" src="https://via.placeholder.com/104x104" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Oversee Quality Control</div>
+                </div>
+                <div className="w-[221px] h-[234px] left-[614px] top-[682px] absolute">
+                <div className="w-[170px] h-[170px] left-[26px] top-0 absolute bg-amber-400 rounded-full" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Oversee Safety</div>
+                <img className="w-[111px] h-[111px] left-[57px] top-[29px] absolute" src="https://via.placeholder.com/111x111" />
+                </div>
+                <div className="w-[247px] h-[234px] left-[163px] top-[682px] absolute">
+                <div className="w-[170px] h-[170px] left-[47px] top-0 absolute bg-amber-400 rounded-full" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Manage Workers</div>
+                <img className="w-[107px] h-[107px] left-[78px] top-[29px] absolute" src="https://via.placeholder.com/107x107" />
+                </div>
+                <div className="w-[243px] h-[234px] left-[1018px] top-[323px] absolute">
+                <div className="w-[170px] h-[170px] left-[47px] top-0 absolute bg-amber-400 rounded-full" />
+                <img className="w-[97px] h-[97px] left-[83px] top-[38px] absolute" src="https://via.placeholder.com/97x97" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Manage Projects</div>
+                </div>
+                <div className="w-[254px] h-[234px] left-[588px] top-[323px] absolute">
+                <div className="w-[170px] h-[170px] left-[47px] top-0 absolute bg-amber-400 rounded-full" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Provide Materials</div>
+                <img className="w-[101px] h-[101px] left-[83px] top-[34px] absolute" src="https://via.placeholder.com/101x101" />
+                </div>
+                <div className="w-[264px] h-[234px] left-[158px] top-[323px] absolute">
+                <div className="w-[170px] h-[170px] left-[47px] top-0 absolute bg-amber-400 rounded-full" />
+                <img className="w-[94px] h-[94px] left-[85px] top-[38px] absolute" src="https://via.placeholder.com/94x94" />
+                <div className="left-0 top-[202px] absolute text-black text-[32px] font-normal">Drafting Blueprint</div>
+                </div>
+                <div className="w-[767px] h-[104px] left-[336px] top-[99px] absolute">
+                <div className="w-[767px] h-[30px] left-0 top-[74px] absolute text-black text-2xl font-normal">We Provided Construction Services Based on the Request of the Client</div>
+                <div className="w-[368px] h-[54px] left-[222px] top-0 absolute text-black text-5xl font-normal">OUR SERVICES</div>
+                </div>
+            </div>
+            <div className="w-[1440px] h-[614px] relative">
+                <img className="w-[1440px] h-[614px] left-0 top-0 absolute" src="https://via.placeholder.com/1440x614" />
+                <div className="w-[1440px] h-[614px] left-0 top-0 absolute bg-black bg-opacity-30" />
+                <div className="w-[307px] h-[45.05px] left-[557px] top-[52.39px] absolute text-white text-[40px] font-normal">OUR PROJECTS</div>
+                <div className="w-[650px] h-12 left-[52px] top-[556px] absolute text-white text-[40px] font-normal">Duplex Apartment Goodway Street</div>
+                <img className="w-[124px] h-[56.65px] left-[1387.65px] top-[256px] absolute origin-top-left rotate-90" src="https://via.placeholder.com/124x57" />
+                <img className="w-[129.92px] h-[56.65px] left-[45px] top-[383.49px] absolute origin-top-left -rotate-90" src="https://via.placeholder.com/130x57" />
+            </div>
+            <div className="w-[1440px] h-[493px] relative">
+                <div className="w-[1440px] h-[493px] left-0 top-0 absolute bg-neutral-800" />
+                <div className="w-[191px] h-[74px] left-[65px] top-[56px] absolute">
+                <img className="w-[74px] h-[74px] left-0 top-0 absolute" src="https://via.placeholder.com/74x74" />
+                <div className="left-[90px] top-[26px] absolute text-white text-5xl font-normal">LIFT</div>
+                </div>
+                <div className="left-[334px] top-[207px] absolute text-white text-base font-normal">CONTACT</div>
+                <div className="left-[336px] top-[175px] absolute text-white text-base font-normal">PROJECT</div>
+                <div className="left-[336px] top-[144px] absolute text-white text-base font-normal">SERVICES</div>
+                <div className="left-[334px] top-[112px] absolute text-white text-base font-normal">ABOUT</div>
+                <div className="left-[580px] top-[450px] absolute text-white text-base font-normal">COPYRIGHT 2023 - ALL RIGHT RESERVED</div>
+                <div className="left-[336px] top-[82px] absolute text-white text-base font-normal">HOME </div>
+                <div className="w-[405px] h-[69px] left-[620px] top-[171px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-[2px] absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[323px] h-10 left-[82px] top-[12px] absolute text-white text-base font-normal">EMAIL<br/><br/>SUPPORT@LIFTMAIL.COM</div>
+                <div className="w-[69px] h-[69px] left-0 top-0 absolute">
+                    <img className="w-[69px] h-[69px] left-0 top-0 absolute" src="https://via.placeholder.com/69x69" />
+                    <div className="w-[52px] h-11 left-[14px] top-[12px] absolute bg-white" />
+                </div>
+                </div>
+                <div className="w-[406px] h-[67px] left-[619px] top-[66px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[323px] h-10 left-[83px] top-[15px] absolute text-white text-base font-normal">PHONE<br/><br/>+1 0123456789</div>
+                <div className="w-[29.60px] h-10 left-[19px] top-[14px] absolute">
+                    <img className="w-[29.60px] h-10 left-0 top-0 absolute" src="https://via.placeholder.com/30x40" />
+                    <div className="w-[33.60px] h-[44.80px] left-[-4px] top-[-3.20px] absolute bg-white" />
+                </div>
+                </div>
+                <div className="w-[67px] h-[67px] left-[1317px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-12 h-12 left-[10px] top-[12px] absolute" src="https://via.placeholder.com/48x48" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1230px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-[47px] h-[31px] left-[10px] top-[21px] absolute" src="https://via.placeholder.com/47x31" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1141px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-[43px] h-[43px] left-[12px] top-[12px] absolute" src="https://via.placeholder.com/43x43" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1054px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[41.67px] h-[36.81px] left-[12.18px] top-[15.23px] absolute">
+                    <img className="w-[41.67px] h-[36.81px] left-0 top-0 absolute" src="https://via.placeholder.com/42x37" />
+                    <div className="w-[41.67px] h-[36.81px] left-0 top-0 absolute bg-white" />
+                </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
---
+++ src/pages/Project.js
@@ -0,0 +1,95 @@
+function Project(){
+    return (
+        <div className="w-[1440px] h-[2398px] relative bg-white">
+            <div className="w-[1440px] h-[493px] left-0 top-[1905px] absolute">
+                <div className="w-[1440px] h-[493px] left-0 top-0 absolute bg-neutral-800" />
+                <div className="w-[191px] h-[74px] left-[65px] top-[56px] absolute">
+                <img className="w-[74px] h-[74px] left-0 top-0 absolute" src="https://via.placeholder.com/74x74" />
+                <div className="left-[90px] top-[26px] absolute text-white text-5xl font-normal">LIFT</div>
+                </div>
+                <div className="left-[334px] top-[207px] absolute text-white text-base font-normal">CONTACT</div>
+                <div className="left-[336px] top-[175px] absolute text-white text-base font-normal">PROJECT</div>
+                <div className="left-[336px] top-[144px] absolute text-white text-base font-normal">SERVICES</div>
+                <div className="left-[334px] top-[112px] absolute text-white text-base font-normal">ABOUT</div>
+                <div className="left-[580px] top-[450px] absolute text-white text-base font-normal">COPYRIGHT 2023 - ALL RIGHT RESERVED</div>
+                <div className="left-[336px] top-[82px] absolute text-white text-base font-normal">HOME </div>
+                <div className="w-[405px] h-[69px] left-[620px] top-[171px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-[2px] absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[323px] h-10 left-[82px] top-[12px] absolute text-white text-base font-normal">EMAIL<br/><br/>SUPPORT@LIFTMAIL.COM</div>
+                <div className="w-[69px] h-[69px] left-0 top-0 absolute">
+                    <img className="w-[69px] h-[69px] left-0 top-0 absolute" src="https://via.placeholder.com/69x69" />
+                    <div className="w-[52px] h-11 left-[14px] top-[12px] absolute bg-white" />
+                </div>
+                </div>
+                <div className="w-[406px] h-[67px] left-[619px] top-[66px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[323px] h-10 left-[83px] top-[15px] absolute text-white text-base font-normal">PHONE<br/><br/>+1 0123456789</div>
+                <div className="w-[29.60px] h-10 left-[19px] top-[14px] absolute">
+                    <img className="w-[29.60px] h-10 left-0 top-0 absolute" src="https://via.placeholder.com/30x40" />
+                    <div className="w-[33.60px] h-[44.80px] left-[-4px] top-[-3.20px] absolute bg-white" />
+                </div>
+                </div>
+                <div className="w-[67px] h-[67px] left-[1317px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-12 h-12 left-[10px] top-[12px] absolute" src="https://via.placeholder.com/48x48" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1230px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-[47px] h-[31px] left-[10px] top-[21px] absolute" src="https://via.placeholder.com/47x31" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1141px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <img className="w-[43px] h-[43px] left-[12px] top-[12px] absolute" src="https://via.placeholder.com/43x43" />
+                </div>
+                <div className="w-[67px] h-[67px] left-[1054px] top-[64px] absolute">
+                <div className="w-[67px] h-[67px] left-0 top-0 absolute bg-zinc-700 rounded-[10px]" />
+                <div className="w-[41.67px] h-[36.81px] left-[12.18px] top-[15.23px] absolute">
+                    <img className="w-[41.67px] h-[36.81px] left-0 top-0 absolute" src="https://via.placeholder.com/42x37" />
+                    <div className="w-[41.67px] h-[36.81px] left-0 top-0 absolute bg-white" />
+                </div>
+                </div>
+            </div>
+            <div className="w-[1440px] h-[1303px] left-0 top-[602px] absolute">
+                <div className="w-[1440px] h-[1303px] left-0 top-0 absolute bg-white" />
+                <div className="w-[636px] h-[579px] left-[742px] top-[672px] absolute">
+                <img className="w-[636px] h-[579px] left-0 top-0 absolute" src="https://via.placeholder.com/636x579" />
+                <div className="w-[611px] h-[34px] left-[25px] top-[525px] absolute text-white text-4xl font-normal">Beach House Dockerway Street</div>
+                </div>
+                <div className="w-[637px] h-[579px] left-[65px] top-[672px] absolute">
+                <img className="w-[637px] h-[579px] left-0 top-0 absolute" src="https://via.placeholder.com/637x579" />
+                <div className="w-[611px] h-[34px] left-[22px] top-[518px] absolute text-white text-4xl font-normal">Single House Sideway Suburb</div>
+                </div>
+                <div className="w-[636px] h-[579px] left-[742px] top-[52px] absolute">
+                <img className="w-[636px] h-[579px] left-0 top-0 absolute" src="https://via.placeholder.com/636x579" />
+                <div className="w-[611px] h-[34px] left-[22px] top-[528px] absolute text-white text-4xl font-normal">Modern House Betterway Street</div>
+                </div>
+                <div className="w-[637px] h-[579px] left-[65px] top-[52px] absolute">
+                <img className="w-[637px] h-[579px] left-0 top-0 absolute" src="https://via.placeholder.com/637x579" />
+                <div className="w-[620px] h-[34px] left-[13px] top-[528px] absolute text-white text-4xl font-normal">Duplex Apartment Goodway Street</div>
+                </div>
+            </div>
+            <div className="w-[1440px] h-[516px] left-0 top-[86px] absolute">
+                <img className="w-[1440px] h-[516px] left-0 top-0 absolute" src="https://via.placeholder.com/1440x516" />
+                <div className="w-[1440px] h-[487px] left-0 top-[29px] absolute" />
+                <div className="w-[1440px] h-[487px] left-0 top-[29px] absolute bg-black bg-opacity-60" />
+                <div className="left-[370px] top-[209px] absolute text-white text-[64px] font-normal">COMPLETED PROJECTS</div>
+                <div className="w-[234px] h-[68px] left-[610px] top-[366px] absolute bg-zinc-600 bg-opacity-0 border border-white" />
+                <div className="left-[642px] top-[384px] absolute text-white text-[32px] font-normal">SEE MORE</div>
+            </div>
+            <div className="w-[1440px] h-[115px] left-0 top-0 absolute">
+                <div className="w-[1440px] h-[115px] left-0 top-0 absolute bg-neutral-800" />
+                <div className="w-[191px] h-[74px] left-[28px] top-[12px] absolute">
+                <img className="w-[74px] h-[74px] left-0 top-0 absolute" src="https://via.placeholder.com/74x74" />
+                <div className="left-[90px] top-[26px] absolute text-amber-400 text-5xl font-normal">LIFT</div>
+                </div>
+                <div className="left-[449px] top-[46px] absolute text-amber-400 text-2xl font-normal">HOME</div>
+                <div className="left-[1021px] top-[46px] absolute text-amber-400 text-2xl font-normal">ABOUT</div>
+                <div className="left-[626px] top-[46px] absolute text-amber-400 text-2xl font-normal">SERVICES</div>
+                <div className="left-[835px] top-[46px] absolute text-white text-2xl font-normal">PROJECT</div>
+                <div className="w-[116px] h-1 left-[830px] top-[74px] absolute bg-amber-400" />
+            </div>
+        </div>
+    )
+}
+
+export default Project
