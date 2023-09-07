ee6ac2b5ddb060c90ec6f468fe13569daa573c5f
ginrone <neirobungin@gmail.com>
2023-09-07 22:45:15 +0700

    added projects page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -4,20 +4,37 @@ import Footer from './Footer';
 import projects from './img/projects.jpg';
 
 import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
 
 function Project(){
     return (
-        <div>
+        <div className="overflow-hidden">
             <Header/>
             <div className="w-full h-96 relative">
                 <img className="w-full h-96 object-cover absolute" src={projects} />
                 <div className="w-full h-96 bg-black bg-opacity-60 absolute" />
-                <div className="w-full flex justify-center mt-32 absolute text-white mx-6 text-4xl lg:text-6xl">OUR PROJECTS</div>
+                <div className="w-full flex justify-center mt-32 absolute text-white text-4xl lg:text-6xl">OUR PROJECTS</div>
             </div>
-            <div className="w-full relative">
-                <div className="w-full flex justify-between">
-                    <img className="w-2/5 h-2/5 ml-6 mt-6" src={img1}></img>
-                    <img className="w-2/5 h-2/5 mr-6 mt-6" src={img1}></img>
+            <div className="w-[98%] ml-[1%] mt-[1%] mb-[1%] relative">
+                <div className="md:grid grid-cols-2 gap-4">
+                    <div>
+                        <div className="absolute flex justify-center mt-[33%] ml-6 text-white text-3xl"> Duplex Apartment Goodway Street </div>
+                        <img className="h-full w-full" src={img1}></img>
+                    </div>
+                    <div>
+                        <div className="absolute flex justify-center mt-[33%] ml-6 text-white text-3xl"> Modern House Betterway Street </div>
+                        <img className="h-full w-full" src={img2}></img>
+                    </div>
+                    <div>
+                        <div className="absolute flex justify-center mt-[30%] ml-6 text-white text-3xl"> Single House Sideway Suburb </div>
+                        <img className="h-full w-full" src={img3}></img>
+                    </div>
+                    <div>
+                        <div className="absolute flex justify-center mt-[30%] ml-6 text-white text-3xl"> Beach House Dockerway Street </div>
+                        <img className="h-full w-full" src={img4}></img>
+                    </div>
                 </div>
             </div>
             <Footer/>
