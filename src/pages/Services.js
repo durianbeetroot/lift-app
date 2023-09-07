f8726b40e8b41d28547b071a5a6c61b2145dec03
ginrone <neirobungin@gmail.com>
2023-09-08 00:39:34 +0700

    added service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,13 +1,64 @@
 import Header from './Header';
 import Footer from './Footer';
 
+import services from './img/service1.jpg';
+
+import sv_blueprint from './img/sv_blueprint.jpg';
+
 function Services(){
     return (
-        <div>
+        <div className="overflow-hidden">
             <Header/>
             <div className="w-full h-96 relative">
+                <img className="w-full h-96 object-cover absolute" src={services} />
                 <div className="w-full h-96 bg-black bg-opacity-60 absolute" />
-                <div className="w-full flex justify-center mt-32 absolute text-white mx-6 text-4xl lg:text-6xl">ABOUT US</div>
+                <div className="w-full flex justify-center mt-32 absolute text-white text-4xl lg:text-6xl">OUR SERVICES</div>
+            </div>
+            <div className="w-[98%] ml-[1%] mt-[1%] mb-[1%] relative">
+                <div className="grid grid-cols-2 gap-4">
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="mt-36">
+                        <div className="flex justify-center text-black text-6xl"> Drafting Blueprint </div>
+                        <div className="flex justify-center text-black text-2xl "> Turn your vision into reality with our precise drafting blueprint services, combining creativity and technical expertise to bring your ideas to life </div>
+                    </div>
+                    <div className="">
+                        <img className="h-full w-full" src={sv_blueprint}></img>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
